d65806657d6ab4644630fe885bb35eb2135aaaff
OOXXXX <zhupx2003@126.com>
2019-08-19 21:54:03 +0800

    Update

--- demo.js
+++ demo.js
@@ -2,3 +2,6 @@ $ui.alert({
     title: "Demo",
     message: "Hello world",
 });
+console.info("Hi JsBox")
+console.info("Hello, World!")
+//print to comsole
--- xxx.js
+++ xxx.js
@@ -1,1 +1,23 @@
-$ui.alert("Hello, World!")
+$ui.render({
+    views: [
+      {
+        type: "button",
+        props: {
+          title: "Button"
+        },
+        layout: function(make, view) {
+          make.center.equalTo(view.super)
+          make.width.equalTo(64)
+        },
+        events: {
+          tapped: function(sender) {
+            $ui.toast("Tapped")
+            $app.openBrowser({
+                type: 10000,
+                url: "https://www.apple.com/cn/"
+              })
+          }
+        }
+      }
+    ]
+})  
