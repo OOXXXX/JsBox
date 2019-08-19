b356207c3c2d7a07fd8d8e22abe6594dcbeee8ad
OOXXXX <zhupx2003@126.com>
2019-08-20 00:27:27 +0800

    Update

--- Canvas.js
+++ Canvas.js
@@ -7,8 +7,12 @@ $ui.render({
           icon: "024", // Or you can use icon name
           symbol: "checkmark.seal", // SF symbols are supported
           handler: function() {
-            $ui.alert("Tapped!")
-            $device.taptic(4);
+            $ui.toast("Hey!")
+            $device.taptic(4)
+            $ui.preview({
+              title: "URL",
+              url: "https://www.apple.com/ac/ac-video-posterframe/1.0/images/ac-video-poster_848x480_2x.jpg"
+            });
           }
         }
       ]
